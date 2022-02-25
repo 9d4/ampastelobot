a65f9ce1d8138ba5345e5b099a75e3588f9840c0
traper <traperwaze@gmail.com>
2022-02-25 08:59:05 +0700

    add Do() test

--- app/httprequest/httprequest_test.go
+++ app/httprequest/httprequest_test.go
@@ -1,21 +1,19 @@
 package httprequest
 
 import (
-	"log"
 	"testing"
 )
 
 func TestDoSimple(t *testing.T) {
-	statusCode, err := NewSimpleRequest("https://google.com").DoSimple()
+	_, err := NewSimpleRequest("https://google.com").DoSimple()
 	if err != nil {
 		t.Error(err)
 	}
+}
 
-	res, err := NewSimpleRequest("https://google.com").Do()
+func TestDo(t *testing.T) {
+	_, err := NewSimpleRequest("https://google.com").Do()
 	if err != nil {
 		t.Error(err)
 	}
-
-	log.Println("Status Code:", statusCode)
-	log.Println("Response:", res)
 }
