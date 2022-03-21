d6d120cd8eb8659d1c5dbaafb33ba142da094a07
Dimas Ananda <traperwaze@gmail.com>
2022-03-21 14:58:47 +0700

    rewrite matchers, now use matcher instead
    Signed-off-by: Dimas Ananda <traperwaze@gmail.com>

--- matcher/matcher.go
+++ matcher/matcher.go
@@ -25,16 +25,11 @@ func (m *Matcher) Match() {
 
 	actuator.Add(session.Middleware)
 
-	// actuator.Add(matchers.CallbackQueryAction)
+	actuator.Add(m.CallbackQueryAction)
 	actuator.Add(m.CommandAction)
-	// actuator.Add(matchers.TextAction)
+	actuator.Add(m.TextAction)
 
 	actuator.Exec()
-}
-
-func (m *Matcher) MatchNew(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
-	matcher := New(bot, update)
-	matcher.Match()
 }
 
 func (m *Matcher) Command(botUpdate *action.BotUpdate) {
@@ -61,5 +56,40 @@ func (m *Matcher) CommandAction(botUpdate *action.BotUpdate) bool {
 		return false
 	}
 
+	return true
+}
+
+func (m *Matcher) CallbackQuery(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	cbQuery := update.CallbackQuery
+	cbData := cbQuery.Data
+
+	// to make sure bot not hanging, send ack to the bot
+	bot.Request(tgbotapi.NewCallback(cbQuery.ID, cbData))
+
+	// to make everything clear, delete the callback message from bot
+	delMsgConf := tgbotapi.NewDeleteMessage(cbQuery.Message.Chat.ID, cbQuery.Message.MessageID)
+	bot.Request(delMsgConf)
+
+	// then do the rest
+}
+
+// CallbackQuery wrapper that supports ActionFunc
+func (m *Matcher) CallbackQueryAction(botUpdate *action.BotUpdate) bool {
+	bot, update := botUpdate.Bot, botUpdate.Update
+
+	if update.CallbackQuery != nil {
+		m.CallbackQuery(bot, *update)
+		return false
+	}
+
+	return true
+}
+
+func (m *Matcher) Text(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	// not implemented yet
+}
+
+func (m *Matcher) TextAction(botUpdate *action.BotUpdate) bool {
+	m.Text(botUpdate.Bot, *botUpdate.Update)
 	return true
 }
